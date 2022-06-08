191cbb9f482189cc8bdf761fa38061d65599b82a
Francesco <33670163+Fenerator@users.noreply.github.com>
2022-06-08 15:42:05 +0200

    exact match accuracy and levenstein edit distance

---
+++ 1_inflection.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+from Levenshtein import distance as lev
+
+
+def read_file(file):
+    with open(file, 'r', encoding='utf-8') as infile:
+        # or without \n lines = [line.rstrip('\n') for line in infile]
+        lines = infile.readlines()
+        print(len(lines))
+
+        # keep only the second part of the line after '\t'
+        content = [line.rstrip().split('\t')[1] for line in lines]
+
+    return content
+
+
+def calculate_matching(gold, predictions):
+    """
+    Args:
+        gold (list of str)
+        predictions (list of str)
+
+    Returns:
+        (float, list): proportion of matching predictions, position of matching predictions
+    """
+    matching = []  # denotes whether predictions at the position are matching
+    lev_distances = []  # stores edit distances per sentence
+    z = 0
+    for i, j in zip(gold, predictions):
+        if i == j:
+            z += 1
+            matching.append(1)
+        else:
+            matching.append(0)
+
+        lev_distances.append(calculate_levensthein(i, j))
+
+    mean_distance = sum(lev_distances)/len(lev_distances)
+
+    return z/len(gold), matching, lev_distances, mean_distance
+
+
+def calculate_levensthein(gold, prediction):
+    try:
+        dist = lev(gold, prediction)
+        return dist
+    except Exception as e:
+        print(f'{e}, gold: {gold}, prediction: {prediction}')
+        return None
+
+
+def main():
+    gold = read_file('deu_gold.txt')
+    predictions = read_file('deu_submitted.txt')
+
+    assert len(gold) == len(
+        predictions), f'Len of predictions ({len(predictions)}) is not same as gold ({len(gold)}) '
+
+    accuracy, _, edit_distances, mean_edit_distance = calculate_matching(
+        gold, predictions)
+
+    print(f'Matching predictions: {accuracy}')
+    print(f'Mean edit distance: {mean_edit_distance}')
+
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+lines cannot be empty
+Input:
+I will give him to her
+Ich werde ihn ihr geben
+
+Gold
+I will give him to her
+Ich werde ihn ihr geben
+
+Calculate exact match accuracy ratings (proportion of correctly predicted lemma and features)
+using ==
+Calcultate Edit Distance (Levenstein distance, averaged over all predictions)
+using levenstein
+'''
